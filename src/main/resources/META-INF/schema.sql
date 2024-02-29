9fb108115a5829cb76004f46c3152bed18ea1a82
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-02-29 09:57:29 +0100

    update docker-compose.yml with schema.sql and data.sql under volumes. Update schema.sql and data.sql to create table and insert dummy data if empty

--- src/main/resources/META-INF/schema.sql
+++ src/main/resources/META-INF/schema.sql
@@ -1,16 +1,14 @@
-CREATE DATABASE movies;
+USE movies;
 
-CREATE TABLE movie (
-                     id INT AUTO_INCREMENT PRIMARY KEY,
-                     uuid BINARY(16) NOT NULL UNIQUE,
-                     title VARCHAR(100) NOT NULL,
-                     releaseYear INT,
-                     director VARCHAR(50),
-                     rating FLOAT,
-                     genre VARCHAR(20)
+CREATE TABLE IF NOT EXISTS movie (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid BINARY(16) NOT NULL UNIQUE,
+    title VARCHAR(100) NOT NULL,
+    releaseYear INT,
+    director VARCHAR(50),
+    rating FLOAT,
+    genre VARCHAR(20)
 );
-CREATE INDEX idx_uuid
-  ON movie (uuid);
 
-DROP TABLE movie;
-SELECT * from movie;
+CREATE INDEX idx_uuid ON movie (uuid);
+
